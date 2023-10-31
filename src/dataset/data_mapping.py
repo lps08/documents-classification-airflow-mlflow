dfbd205d39510acf94c85ca831f53fb6c8b82539
lps08 <lucianolps08@gmail.com>
2023-10-31 15:31:50 -0300

    docstring made for dataset functions

--- src/dataset/data_mapping.py
+++ src/dataset/data_mapping.py
@@ -7,15 +7,24 @@ import unidecode
 # %%
 # find out all the documents subfolder name and track them
 def mapping_files(dataset_path:str=constants.DATASET_PATH, dir_to_csv:str=constants.DATA_DIR):
-    """Find all files from the dataset directory
+    """
+    Create a mapping of file paths within a dataset directory and save it to a CSV file.
 
-    Parameters
-    ----------
-    dataset_path: path to the dataset
+    Parameters:
+    dataset_path (str, optional): The path to the dataset directory. Default is constants.DATASET_PATH.
+    dir_to_csv (str, optional): The directory where the generated CSV file will be saved. Default is constants.DATA_DIR.
 
-    Return
-    ------
-    list: all the files mapped to csv
+    Returns:
+    None
+
+    Example:
+    ```
+    mapping_files(dataset_path="/path/to/dataset", dir_to_csv="/output/directory")
+    ```
+    This function creates a mapping of file paths within a dataset directory. It lists files in subdirectories,
+    associates them with their respective class labels, and saves this mapping as a CSV file. You can specify
+    the dataset path and the directory for saving the CSV file. If the specified output directory does not exist,
+    a ValueError is raised.
     """
     files_dir = defaultdict(list)
     for i in os.listdir(dataset_path):
